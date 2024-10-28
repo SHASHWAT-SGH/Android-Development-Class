825d98facae2c73559c2df24e0f64dbcf808fb1b
SHASHWAT-SGH <vnsshashwat@gmail.com>
2024-10-28 13:26:59 +0530

    permissions

--- app/src/main/java/com/example/basics/PermissionsExample.kt
+++ app/src/main/java/com/example/basics/PermissionsExample.kt
@@ -13,13 +13,13 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class PermissionsExample : AppCompatActivity() {
+    var c =1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_permissions_example)
 
         val btnPermission = findViewById<Button>(R.id.btn_permission)
-        var c =1
 
         btnPermission.setOnClickListener {
 
@@ -29,6 +29,20 @@ class PermissionsExample : AppCompatActivity() {
                 Toast.makeText(this, "Permission Already Granted", Toast.LENGTH_LONG).show()
             }
         }
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == c){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Camera permission granted", Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
